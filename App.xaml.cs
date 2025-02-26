0b40fb347329470f6ccfba9c82f9c10bad60ddd8
RustedFTRain <87017351+RustedFTRain@users.noreply.github.com>
2025-02-26 21:22:11 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TTHKApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
